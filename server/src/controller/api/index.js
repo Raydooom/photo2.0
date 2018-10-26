9c9586e35d1eae2962e979566f43b7d684c54a43
王瑞冬 <wangrd@missfresh.cn>
2018-10-26 15:50:16 +0800

    评论、浏览统计、点赞

--- server/src/controller/api/index.js
+++ server/src/controller/api/index.js
@@ -59,33 +59,6 @@ module.exports = class extends think.Controller {
     this.success(result, "添加文章成功")
   }
 
-  // 删除文章
-  async delArticleAction() {
-    let { id } = this.post();
-    const result = await this.model('admin/index').delArticle({ id: ['=', id] });
-    if (result == 0) {
-      this.fail("删除的文章不存在", "删除失败")
-    } else {
-      this.success(result, "删除文章成功")
-    }
-  }
-
-  // 更新文章
-  async updateArticleAction() {
-    let currentTime = new Date();
-    let { id, title, coverUrl, kind, homeShow, content } = this.post();
-    let data = {
-      article_title: title,
-      cover_img: coverUrl,
-      kind_id: kind,
-      home_show: homeShow,
-      content: content,
-      update_date: think.datetime(currentTime)
-    };
-    const result = await this.model('admin/index').updateArticle({ id: id }, data);
-    this.success(result, "更新成功")
-  }
-
   // 根据id获取文章信息
   async getArticleAction() {
     let { id } = this.post();
@@ -107,4 +80,61 @@ module.exports = class extends think.Controller {
       this.success(result, "获取文章评论成功")
     }
   }
+
+  // 发布评论
+  async addArticleCommentAction() {
+    let { id, userId, commentText } = this.post();
+    let data = {
+      article_id: id,
+      comment_user_id: userId,
+      comment_text: commentText,
+      create_date: think.datetime(new Date())
+    }
+    const addCommentResult = await this.model('admin/index').addArticleComment(data);
+
+    if (addCommentResult == 0) {
+      this.fail("文章不存在", "查询失败")
+    } else {
+      const commentListResult = await this.model('admin/index').getArticleComment({ article_id: ['=', id] });
+      let data = {
+        comments: commentListResult.length
+      }
+      // 更新评论数量
+      await this.model('admin/index').updateArticle({ id: id }, data);
+      this.success(addCommentResult, "获取文章评论成功")
+    }
+  }
+
+  // 浏览量统计
+  async viewCountAction() {
+    let { id } = this.post();
+    const article = await this.model('admin/index').getArticle({ id: id });
+    let data = {
+      views: article[0].views + 1
+    }
+    const result = await this.model('admin/index').viewCount({ id: id }, data);
+    if (result == 0) {
+      this.fail("浏览量统计失败")
+    } else {
+      this.success({ views: article[0].views + 1 }, "浏览量统计成功")
+    }
+  }
+
+  // 点赞统计
+  async praiseCountAction() {
+    let { id, userId } = this.post();
+    const article = await this.model('admin/index').getArticle({ id: id });
+    let praiseList = article[0].praises ? article[0].praises.split(",") : [];
+    if (praiseList.includes(userId)) {
+      this.fail("点赞失败，已点过", { praises: praiseList.length })
+    } else {
+      praiseList.unshift(userId);
+      let data = {
+        praises: praiseList.toString()
+      }
+      const result = await this.model('admin/index').praiseCount({ id: id }, data);
+      this.success({ praises: praiseList.length }, "点赞成功！")
+    }
+  }
+
 }
